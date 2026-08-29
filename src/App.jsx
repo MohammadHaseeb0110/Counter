cd32de0446221d8fd2136f6fa07f68f51fec721b
MohammadHaseeb0110 <kazuhakaedahara461@gmail.com>
2026-08-29 14:44:58 +0500

    Counter v1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1> Counter</h1>
+      <h2 className="Counter"> {count} </h2>
+
+      <button className="Increase" onClick = { () => setCount(count + 1)}> + </button>
+      <button className="Decrease" onClick = { () => setCount(count - 1)}> - </button>
+    </div>
+  )
+};
+
+
+  export default App;
