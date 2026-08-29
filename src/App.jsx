ab366af6d8f437646b47d31e87e08fefd5cd6181
MohammadHaseeb0110 <kazuhakaedahara461@gmail.com>
2026-08-29 14:57:45 +0500

    Counter v2

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <h1> Counter</h1>
       <h2 className="Counter"> {count} </h2>
-
+      <div className="Buttons">
       <button className="Increase" onClick = { () => setCount(count + 1)}> + </button>
       <button className="Decrease" onClick = { () => setCount(count - 1)}> - </button>
+      </div>
     </div>
   )
 };
